67afb25b63963f71e7306911b6c47318e481c372
Altieres Lopes <altiereslopes@webgoal.com.br>
2016-04-09 11:50:30 -0300

    Adicionado campo abstract e funcao de persistir pergunta e resposta

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :publisher, :owner, :email, :abstract)
+      params.require(:book).permit(:title, :author, :publisher, :owner, :email, :abstract, :question, :answer)
     end
 end
