7661c8a628a1faed06cacb418293790f8c62b6bd
Rafael Valério <rafael@webgoal.com.br>
2016-04-09 11:08:54 -0300

    Add Fields Question Answer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409125636) do
+ActiveRecord::Schema.define(version: 20160409135658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20160409125636) do
     t.string   "abstract"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "question"
+    t.string   "answer"
   end
 
 end
