1a0aefae2428306b96d13535967cd771b98f98ec
Altieres Lopes <altiereslopes@webgoal.com.br>
2016-04-10 09:46:47 -0300

    Fixed tests on master and layout edit

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -8,5 +8,8 @@ FactoryGirl.define do
     abstract "Long ago, in a time forgotten, a preternatural event threw the seasons out of balance."
     question "Qual o lanche preferido"
     answer "presunto"
+    lending_name "teste"
+    lending_email "teste@teste.com"
+    lending_data "2000-01-01"
   end
 end
