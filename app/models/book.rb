3e6a5132961812bddbefc0ebea10dcb10c53dd9b
Ateliê de Software <atelie@Guilhermes-MacBook-Pro.local>
2016-04-10 09:53:32 -0300

    Corrigido quebra dos testes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,17 +1,17 @@
 class Book < ActiveRecord::Base
-  validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :author, presence: true, length: {minimum: 3, maximum: 50}
-  validates :publisher, presence: true, length: {minimum: 3, maximum: 50}
-  validates :owner, presence: true, length: {minimum: 3, maximum: 50}
-  validates :email, presence: true, length: {minimum: 3, maximum: 100}
+  validates :title, presence: true, length: { minimum:3  }
+  validates :author, presence: true, length: { minimum:3  }
+  validates :publisher, presence: true, length: { minimum:3  }
+  validates :owner, presence: true, length: { minimum:3  }
+  validates :email, presence: true, length: { minimum:3  }
 
-  validates :lending_name, presence: true,  length: {minimum: 5, maximum: 50}
-  validates :lending_email, presence: true,  length: {minimum: 3, maximum: 50}
+  validates :lending_name, presence: true, length: { minimum:3  }
+  validates :lending_email, presence: true, length: { minimum:3  }
   validates :lending_data, presence: true
 
-  validates :abstract,  length: {minimum: 10, maximum: 500}
-  validates :question, presence: true, length: {minimum: 10, maximum: 100}
-  validates :answer, presence: true, length: {minimum: 6, maximum: 10}
+  validates :abstract,  length: {minimum: 10}
+  validates :question, presence: true, length: { minimum:3  }
+  validates :answer, presence: true, length: { minimum:3  }
   validate  :validate_password, on: :update
 
   def validate_password
